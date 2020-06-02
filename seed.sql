b61c5d3cb2789f3672312ace220cc0ac9122f093
Laureni Wilkinson <las348@nyu.edu>
2020-06-02 16:09:11 -0400

    addEmployee

--- seed.sql
+++ seed.sql
@@ -3,7 +3,7 @@ values ('Sales'), ('Engineering'), ('Finance'), ('Legal');
 
 select * from department;
 
-insert into role (title, salary, department_id)
+insert into roles (title, salary, department_id)
 values ('Sales Lead', 100000, 1), ('Accountant', 120000, 3), ('CTO', 500000, 2), ('Lawyer', 150000, 2);
 
 select * from roles;
@@ -12,3 +12,10 @@ insert into employee (first_name, last_name, role_id)
 values ('Chu', 'Chu',1), ('Kobe', 'Kobe', 2), ('Dan', 'Dan', 3), ('Steve', 'Steve', 4);
 
 select * from employee;
+
+select title, salary, name from role 
+inner join department on role.department_id=department.id;
+
+select first_name, last_name, title, salary, name from employee 
+inner join role on employee.role_id=role.id 
+inner join department on role.department_id=department.id;
